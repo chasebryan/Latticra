3428a646f6530068d0e050c758ba1d0d4e0b0d88
Chase Bryan <281080815+Bryforge@users.noreply.github.com>
2026-05-15 15:13:04 -0500

    Add L-UI AST public API

--- include/latticra/l_ui_parser.h
+++ include/latticra/l_ui_parser.h
@@ -15,6 +15,13 @@ extern "C" {
 #define LATTICRA_L_UI_DIAGNOSTIC_MESSAGE_MAX 128u
 #define LATTICRA_L_UI_DIAGNOSTIC_HINT_MAX 128u
 #define LATTICRA_L_UI_DIAGNOSTIC_REPORT_MAX 1280u
+#define LATTICRA_L_UI_AST_RAIL_MAX 16u
+#define LATTICRA_L_UI_AST_FIELD_MAX 64u
+#define LATTICRA_L_UI_AST_TEXT_MAX 16u
+#define LATTICRA_L_UI_AST_LABEL_MAX 64u
+#define LATTICRA_L_UI_AST_BINDING_MAX 96u
+#define LATTICRA_L_UI_AST_PURPOSE_MAX 128u
+#define LATTICRA_L_UI_AST_REPORT_MAX 2048u
 
 typedef enum {
     LATTICRA_L_UI_PARSE_OK = 0,
@@ -43,6 +50,15 @@ typedef enum {
     LATTICRA_L_UI_DIAGNOSTIC_ERROR = 1,
     LATTICRA_L_UI_DIAGNOSTIC_INTERNAL = 2
 } latticra_l_ui_diagnostic_severity_t;
+
+typedef enum {
+    LATTICRA_L_UI_AST_NODE_CARD = 0,
+    LATTICRA_L_UI_AST_NODE_RAIL = 1,
+    LATTICRA_L_UI_AST_NODE_FIELD = 2,
+    LATTICRA_L_UI_AST_NODE_TEXT = 3,
+    LATTICRA_L_UI_AST_NODE_BINDING = 4,
+    LATTICRA_L_UI_AST_NODE_UNKNOWN = 5
+} latticra_l_ui_ast_node_kind_t;
 
 typedef struct {
     size_t start_offset;
@@ -88,9 +104,59 @@ typedef struct {
     int hardware_allowed;
 } latticra_l_ui_diagnostic_t;
 
+typedef struct {
+    char name[LATTICRA_L_UI_AST_LABEL_MAX];
+    char purpose[LATTICRA_L_UI_AST_PURPOSE_MAX];
+    char effect[LATTICRA_L_UI_LABEL_MAX];
+    char boundary[LATTICRA_L_UI_LABEL_MAX];
+    latticra_l_ui_source_span_t span;
+    size_t rail_count;
+    size_t field_count;
+    size_t text_count;
+} latticra_l_ui_ast_card_t;
+
+typedef struct {
+    char name[LATTICRA_L_UI_AST_LABEL_MAX];
+    latticra_l_ui_source_span_t span;
+    size_t first_field_index;
+    size_t field_count;
+    size_t first_text_index;
+    size_t text_count;
+} latticra_l_ui_ast_rail_t;
+
+typedef struct {
+    char name[LATTICRA_L_UI_AST_LABEL_MAX];
+    char binding[LATTICRA_L_UI_AST_BINDING_MAX];
+    latticra_l_ui_source_span_t span;
+    latticra_l_ui_source_span_t binding_span;
+} latticra_l_ui_ast_field_t;
+
+typedef struct {
+    char value[LATTICRA_L_UI_AST_PURPOSE_MAX];
+    latticra_l_ui_source_span_t span;
+} latticra_l_ui_ast_text_t;
+
+typedef struct {
+    latticra_l_ui_parse_result_t parse_result;
+    latticra_l_ui_ast_card_t card;
+    latticra_l_ui_ast_rail_t rails[LATTICRA_L_UI_AST_RAIL_MAX];
+    latticra_l_ui_ast_field_t fields[LATTICRA_L_UI_AST_FIELD_MAX];
+    latticra_l_ui_ast_text_t texts[LATTICRA_L_UI_AST_TEXT_MAX];
+    size_t rail_count;
+    size_t field_count;
+    size_t text_count;
+    int no_effect;
+    int execution_allowed;
+    int mutation_allowed;
+    int server_allowed;
+    int recovery_allowed;
+    int hardware_allowed;
+} latticra_l_ui_ast_result_t;
+
 const char *latticra_l_ui_parse_error_label(latticra_l_ui_parse_error_t error);
 const char *latticra_l_ui_diagnostic_severity_label(
     latticra_l_ui_diagnostic_severity_t severity);
+const char *latticra_l_ui_ast_node_kind_label(latticra_l_ui_ast_node_kind_t kind);
 
 latticra_status_t latticra_l_ui_parse_source(
     const char *source,
@@ -108,6 +174,16 @@ latticra_status_t latticra_l_ui_diagnostic_from_parse_result(
 
 latticra_status_t latticra_l_ui_diagnostic_report(
     const latticra_l_ui_diagnostic_t *diagnostic,
+    char *buffer,
+    size_t buffer_len);
+
+latticra_status_t latticra_l_ui_parse_ast(
+    const char *source,
+    size_t source_len,
+    latticra_l_ui_ast_result_t *ast);
+
+latticra_status_t latticra_l_ui_ast_report(
+    const latticra_l_ui_ast_result_t *ast,
     char *buffer,
     size_t buffer_len);
 
