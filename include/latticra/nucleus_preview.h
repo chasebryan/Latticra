e4736c8894181bda02c1acd5c83b687f65ab85d0
Chase Bryan <281080815+Bryforge@users.noreply.github.com>
2026-05-15 03:33:16 -0500

    Add Nucleus preview report API

--- include/latticra/nucleus_preview.h
+++ include/latticra/nucleus_preview.h
@@ -8,6 +8,8 @@
 extern "C" {
 #endif
 
+#define LATTICRA_NUCLEUS_REPORT_MAX 768u
+
 typedef enum {
     LATTICRA_REQUEST_STATE_REPORT = 0,
     LATTICRA_REQUEST_TRANSITION_PREVIEW = 1,
@@ -51,6 +53,11 @@ latticra_status_t latticra_nucleus_classify_preview(
     latticra_request_kind_t request_kind,
     latticra_effect_t requested_effect,
     latticra_nucleus_preview_t *preview);
+
+latticra_status_t latticra_nucleus_preview_report(
+    const latticra_nucleus_preview_t *preview,
+    char *buffer,
+    size_t buffer_len);
 
 #ifdef __cplusplus
 }
