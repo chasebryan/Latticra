b938e80efe841c71905bfaaa85b7e22f8ef1d772
Chase Bryan <281080815+Bryforge@users.noreply.github.com>
2026-05-15 19:04:05 -0500

    Add parser string escape diagnostics test runner

---
+++ scripts/test-l-ui-parser-string-escape-diagnostics.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+set -eu
+
+: "${CFLAGS:=-std=c99 -Wall -Wextra -Werror -pedantic}"
+
+cc $CFLAGS \
+  -Iinclude \
+  src/l_ui_parser.c \
+  src/l_ui_parser_ast.c \
+  src/l_ui_parser_diagnostics.c \
+  tests/l_ui_parser_string_escape_diagnostics_invariants.c \
+  -o /tmp/latticra-l-ui-parser-string-escape-diagnostics-invariants
+
+/tmp/latticra-l-ui-parser-string-escape-diagnostics-invariants
