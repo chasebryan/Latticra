297b725f849e2a56613e806cdaed54a550d6ead1
Chase Bryan <281080815+Bryforge@users.noreply.github.com>
2026-05-20 01:14:33 -0500

    Add Nucleus report-only execution status fields

--- include/latticra/nucleus_task.h
+++ include/latticra/nucleus_task.h
@@ -180,6 +180,9 @@ typedef struct {
     char report_alignment[LATTICRA_NUCLEUS_TASK_LABEL_MAX];
     char no_effect_policy[LATTICRA_NUCLEUS_TASK_LABEL_MAX];
     char representation_gate[LATTICRA_NUCLEUS_TASK_LABEL_MAX];
+    char execution_status[LATTICRA_NUCLEUS_TASK_LABEL_MAX];
+    char effect_status[LATTICRA_NUCLEUS_TASK_LABEL_MAX];
+    char runtime_status[LATTICRA_NUCLEUS_TASK_LABEL_MAX];
     latticra_nucleus_task_authority_summary_t authority;
     char source_identity[LATTICRA_NUCLEUS_TASK_SOURCE_IDENTITY_MAX];
     latticra_l_ui_source_span_t source_span;
